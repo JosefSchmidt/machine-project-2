0b5c12025ea49b502246311f48877036c8410b13
Josef <josef.brondum.schmidt@gmail.com>
2021-04-30 15:18:07 +0200

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/machine_project_2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/machine_project_2.dir/main.c.o"
+  "machine_project_2"
+  "machine_project_2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/machine_project_2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
