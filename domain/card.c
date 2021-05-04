08cb1f4b62ede56eb1dfa805eab394bcfc5e058a
lausenen <lau.thomsen.privat@gmail.com>
2021-05-04 19:57:32 +0200

    fixed gitignore

--- domain/card.c
+++ domain/card.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
+#pragma once
 
 // Function declarations
 char getDeckType();
@@ -89,7 +89,6 @@ void createDefaultCardList(char *message) {
         }
 
         strcpy(message, "OK");
-        printHiddenBoard();
     }
 }
 
